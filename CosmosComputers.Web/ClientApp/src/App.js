be452f093850550286ae0ef7a72f837d113f6a4f
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2018-04-23 21:51:12 +0200

    Router

--- CosmosComputers.Web/ClientApp/src/App.js
+++ CosmosComputers.Web/ClientApp/src/App.js
@@ -3,37 +3,41 @@ import './App.css';
 import 'antd/dist/antd.css';
 import SideMenu from './components/SideMenu';
 import { Layout } from 'antd';
+import {
+  BrowserRouter as Router,
+  Route  
+} from 'react-router-dom';
+import Computers from './components/pages/Computers';
+import Cases from './components/pages/Cases';
 const { Header, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Layout style={{height: "100%"}}>
-          <Header>
-            <h1 style={{ color: 'rgb(255, 255, 255, 0.65)', width: '100%' }}>
-              Cosmos Computers
+      <Router>
+        <div className="App">
+          <Layout style={{ height: "100%" }}>
+            <Header>
+              <h1 style={{ color: 'rgb(255, 255, 255, 0.65)', width: '100%' }}>
+                Cosmos Computers
             </h1>
-          </Header>
-          <Layout style={{height: "100%"}}>
-            <Sider 
-            style={{height:"100%"}}             
-              breakpoint="sm"
-              collapsedWidth="0"
-            >
-              <SideMenu />
-            </Sider>
-            <Content>
-              qweqweqweqweeeeeeeeeasdfasdfadsfsadf
-              asdfasdfffffffffffffffffffffffffffff
-              asdfasdfffffffffffffffffffffffffffffweqr
-              qwetqwettttttttttttttttttttttttttttttttt
-              sdfgsdfgsdfgsdfgsdfgsfdgsdfgsdfgsdfgsdfgfsdgsfdg
-              AWERSERYSERT
-            </Content>
-          </Layout>          
-        </Layout>
-      </div>
+            </Header>
+            <Layout style={{ height: "100%" }}>
+              <Sider
+                style={{ height: "100%" }}
+                breakpoint="sm"
+                collapsedWidth="0"
+              >
+                <SideMenu />
+              </Sider>
+              <Content>
+                <Route exact path="/" Component={Computers} />
+                <Route path="/Cases" Component={Cases} />
+              </Content>
+            </Layout>
+          </Layout>
+        </div>
+      </Router>
     );
   }
 }
--- CosmosComputers.Web/ClientApp/src/components/SideMenu.jsx
+++ CosmosComputers.Web/ClientApp/src/components/SideMenu.jsx
@@ -1,17 +1,42 @@
 import * as React from 'react';
 import { Menu } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
 
 const SideMenu = (props) => (
-    <Menu style={{width: "100%", height: "100%"}} mode="inline" theme="dark">
-        <Menu.Item key="Cases">Cases</Menu.Item>
-        <Menu.Item key="Coolers">Coolers</Menu.Item>
-        <Menu.Item key="Discs">Discs</Menu.Item>
-        <Menu.Item key="GraphicCards">Graphic Cards</Menu.Item>
-        <Menu.Item key="MemoryModules">Memory modules</Menu.Item>
-        <Menu.Item key="Motherboards">Motherboards</Menu.Item>
-        <Menu.Item key="PowerSupplies">Power supplies</Menu.Item>
-        <Menu.Item key="Processors">Processors</Menu.Item>
+    <Menu 
+    style={{ width: "100%", height: "100%" }} 
+    mode="inline" 
+    theme="dark" 
+    defaultSelectedKeys={[props.location.pathname.substr(1)||"Computers"]}
+    >
+        <Menu.Item key="Computers">
+            <Link to="/" onClick={()=>console.log(props)}>Computers</Link>
+        </Menu.Item>
+        <Menu.Item key="Cases">
+            <Link to="/Cases">Cases</Link>
+        </Menu.Item>
+        <Menu.Item key="Coolers">
+            <Link to="/Coolers">Coolers</Link>
+        </Menu.Item>
+        <Menu.Item key="Discs">
+            <Link to="/Discs">Discs</Link>
+        </Menu.Item>
+        <Menu.Item key="GraphicCards">
+            <Link to="/GraphicCards">Graphic Cards</Link>
+        </Menu.Item>
+        <Menu.Item key="MemoryModules">
+            <Link to="/MemoryModules">Memory modules</Link>
+        </Menu.Item>
+        <Menu.Item key="Motherboards">
+            <Link to="/Motherboards">Motherboards</Link>
+        </Menu.Item>
+        <Menu.Item key="PowerSupplies">
+            <Link to="/PowerSupplies">Power supplies</Link>
+        </Menu.Item>
+        <Menu.Item key="Processors">
+            <Link to="/Processors">Processors</Link>
+        </Menu.Item>
     </Menu>
 );
 
-export default SideMenu;
+export default withRouter(SideMenu);
